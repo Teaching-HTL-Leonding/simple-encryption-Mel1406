54c4090ed843586a7f6a18cbb9783a41c3c6adb5
Mel1406 <113634364+Mel1406@users.noreply.github.com>
2023-03-16 09:58:48 +0100

    Add files via upload

---
+++ Program_CeasarCipher.cs
@@ -0,0 +1,64 @@
+﻿#region var
+const string ENCRYPT = "encrypt";
+const string DECRYPT = "decrypt";
+
+char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToArray();
+#endregion
+
+#region program
+bool decision = DecisionEncodeOrDecode();
+int shift = AskForShift();
+if (decision)
+{
+    Console.WriteLine($"Your {ENCRYPT}ed output: {EncryptDecrypt(shift, AskForInput(ENCRYPT), alphabet)}");
+}
+else
+{ 
+    Console.WriteLine($"Your {DECRYPT}d output: {EncryptDecrypt(shift, AskForInput(DECRYPT), alphabet)}");
+}
+#endregion
+
+#region method
+bool DecisionEncodeOrDecode()
+{
+    string decision;
+    do{
+    Console.Write("Do you want to encode[e] or decode[d]? ");
+    decision = Console.ReadLine()!.ToLower();
+    }while (decision != "e" && decision != "d");
+    return decision == "e";
+}
+int AskForShift()
+{
+    string valueString;
+   do
+    {
+        Console.Write("What shift do you want to use? Use - before the number for a left shift. The number has to be between -25 und 25: ");
+        valueString = Console.ReadLine()!;
+    } while (!int.TryParse(valueString, out int result) || int.Parse(valueString) > 25 || int.Parse(valueString) < -25 || int.Parse(valueString) == 0);
+    return int.Parse(valueString); 
+}
+string EncryptDecrypt(int shift, string input, char[] alphabet)
+{
+    string Output = "";
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (char.IsLetter(input[i]))
+        {
+            int letterIndexAlphabet = Array.IndexOf(alphabet, input[i]);
+            if (char.IsUpper(input[i]))
+            {Output += alphabet[(letterIndexAlphabet + shift + 26) % 26].ToString().ToUpper();}
+            else { Output += alphabet[(letterIndexAlphabet + shift + 26) % 26];}
+        }
+        else
+        {Output += input[i];}
+    }
+
+    return Output;
+}
+string AskForInput(string encryptDecrypt)
+{
+    Console.Write($"what do you want to {encryptDecrypt}? ");
+    return Console.ReadLine()!;
+}
+#endregion
---
+++ Program_VigeneCipher.cs
@@ -0,0 +1,80 @@
+﻿#region var
+const string ENCRYPT = "encrypt";
+const string DECRYPT = "decrypt";
+
+char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToArray();
+#endregion
+
+#region program
+bool decision = DecisionEncodeOrDecode();
+if (decision)
+{
+    string input = AskForInput(ENCRYPT);
+    string keyword = AskForKeyword(input);
+    Console.WriteLine($"Your {ENCRYPT}ed output: {EncryptDecrypt(keyword, input, alphabet, true)}");
+}
+else
+{
+    string input = AskForInput(DECRYPT);
+    string keyword = AskForKeyword(input);
+    Console.WriteLine($"Your {DECRYPT}d output: {EncryptDecrypt(keyword, input, alphabet, false)}");
+}
+#endregion
+
+#region method
+bool DecisionEncodeOrDecode()
+{
+    string decision;
+    do
+    {
+        Console.Write("Do you want to encode[e] or decode[d]? ");
+        decision = Console.ReadLine()!.ToLower();
+    } while (decision != "e" && decision != "d");
+    return decision == "e";
+}
+string AskForKeyword(string input)
+{
+    string keyword;
+    string returnKeyword = "";
+    do
+    {
+        Console.Write("Please enter a keyword. Letters only! ");
+        keyword = Console.ReadLine()!.ToLower();
+    } while (!keyword.All(Char.IsLetter));
+    for(int i = 0, j = 0; i < input.Length; i++, j++)
+    {
+        if (j == keyword.Length) {j = 0;}
+        returnKeyword += keyword[j];
+    }
+    return returnKeyword;
+}
+
+string EncryptDecrypt(string keyword, string input, char[] alphabet, bool encode)
+{
+    string Output = "";
+    for (int i = 0, j = 0; i < input.Length; i++, j++)
+    {
+        if (alphabet.Contains(char.Parse(input[i].ToString().ToLower())))
+        {
+            if (char.IsUpper(input[i]))
+            { Output += alphabet[GetShiftedPositionInAlphabet(keyword[j], char.Parse(input[i].ToString().ToLower()), alphabet, encode)].ToString().ToUpper(); }
+            else { Output += alphabet[GetShiftedPositionInAlphabet(keyword[j], char.Parse(input[i].ToString().ToLower()), alphabet, encode)]; }
+        }
+        else
+        { Output += input[i]; j--;}
+    }
+    return Output;
+}
+int GetShiftedPositionInAlphabet(char keyword, char character, char[] alphabet, bool encode)
+{
+    int letterIndexCharacter = Array.IndexOf(alphabet, character);
+    int letterIndexKeyword = Array.IndexOf(alphabet, keyword);
+    if (!encode) { letterIndexKeyword *= -1; }
+    return (26 + letterIndexCharacter + letterIndexKeyword) % 26;
+}
+string AskForInput(string encryptDecrypt)
+{
+    Console.Write($"what do you want to {encryptDecrypt}? ");
+    return Console.ReadLine()!;
+}
+#endregion
